9845224e17f1899fe09ac3e470346b75e8df96b1
nurie <nurie@192.168.1.109>
2019-10-03 19:32:17 +0200

    2º subida

--- EjerciciosJava/src/capitulo02/bloque03/Ejercicio04_CambioDeCompra.java
+++ EjerciciosJava/src/capitulo02/bloque03/Ejercicio04_CambioDeCompra.java
@@ -1,10 +1,40 @@
 package capitulo02.bloque03;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio04_CambioDeCompra {
 
 	public static void main(String[] args) {
 	
-   
-	}
+	String str = JOptionPane.showInputDialog("Introduzca la cantidad del importe");
+	int importe = Integer.parseInt(str);
+	str = JOptionPane.showInputDialog("Introduzca la cantidad recibida");
+	int cantRecibida = Integer.parseInt(str);
+	int cantDevolver = cantRecibida - importe;
 
-}
+		
+			
+		
+	
+	
+
+	
+//	System.out.println("--CAMBIO A DEVOLVER--");
+//			+"\nCantidad a devolver: "+ cantDevolver
+//			+"\nBilletes de 50€: " 
+//			+"\nBilletes de 20€: "
+//			+"\nBilletes de 10€: " 
+//			+"\nBilletes de 5€: " 
+//			+"\nMonedas de 2€: " 
+//			+"\nMonedas de 1€: "
+//			+"\nMonedas de 50cent: " 
+//			+"\nMonedas de 20cent: " 
+//			+"\nMonedas de 10cent: " 
+//			+"\nMonedas de 5cent: " 
+//			+"\nMonedas de 2cent: +" 
+//			+"\nMonedas de 1cent: +");
+
+	}	
+}	
+	
+
