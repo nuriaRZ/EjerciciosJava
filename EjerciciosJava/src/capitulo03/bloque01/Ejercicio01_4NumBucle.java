5a81a518cd5e5bc501c2da0bad496f98f9f54d6c
diurno <diurno@pc-aula>
2019-10-04 13:05:31 +0200

    Ejercicios_Bucles

---
+++ EjerciciosJava/src/capitulo03/bloque01/Ejercicio01_4NumBucle.java
@@ -0,0 +1,17 @@
+package capitulo03.bloque01;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01_4NumBucle {
+
+	public static void main(String[] args) {
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero"));
+		int mayor = 10;
+		
+//		for ( num >= mayor;  ) {
+//			if nu
+//		}
+
+	}
+
+}
