1eb664cb5eb8858bfeb1f0bf17bb52096cc6912d
nurie <nurie@192.168.1.109>
2019-10-07 18:53:04 +0200

    Ejercicios 7/10/19

--- EjerciciosJava/src/capitulo03/bloque02/Ejercicio04_Multiplos_con_Limite.java
+++ EjerciciosJava/src/capitulo03/bloque02/Ejercicio04_Multiplos_con_Limite.java
@@ -2,15 +2,15 @@ package capitulo03.bloque02;
 
 import javax.swing.JOptionPane;
 
-public class Ejercicio03_Multiplos {
+public class Ejercicio04_Multiplos_con_Limite {
 
 	public static void main(String[] args) {
 		
 		int numPedido = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero:"));
-		
+		int limite = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el limite deseado:"));
 		
 		 
-		for (int multiplo=0; (numPedido*multiplo)<100; multiplo++) {
+		for (int multiplo=0; (numPedido*multiplo)<=limite; multiplo++) {
 		
 			System.out.println(numPedido+" x " +multiplo+" = " +(numPedido*multiplo));
 			}
---
+++ EjerciciosJava/src/capitulo03/bloque03/Ejercicio01_PedirNumeros.java
@@ -0,0 +1,34 @@
+package capitulo03.bloque03;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01_PedirNumeros {
+
+	public static void main(String[] args) {
+	
+		int acumNum=0;
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero:\nPulse 0 para terminar"));
+		acumNum+=num;
+		for (int i=0; num!=0; i++) {
+		num = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero:\nPulse 0 para terminar"));
+		
+			if (i==0) {
+				acumNum+=num;
+			}
+			else {
+				if (num!=0) {
+					acumNum+=num;
+				}
+				else {
+					if (num==0) {
+						break;
+					}
+				}
+			}
+			
+		}
+		
+		System.out.println("La suma de los numeros es: "+ acumNum);
+	}
+
+}
