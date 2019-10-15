8202dad67229b90d2cc0b5640f96894940a44cdf
nurie <nurie@192.168.1.109>
2019-10-15 17:14:56 +0200

    arrays3

--- EjerciciosJava/src/capitulo05/bloque02/Ejercicio01_Pasar_Positivo_Negat.java
+++ EjerciciosJava/src/capitulo05/bloque02/Ejercicio01_Pasar_Positivo_Negat.java
@@ -4,7 +4,39 @@ public class Ejercicio01_Pasar_Positivo_Negat {
 
 	public static void main(String[] args) {
 		
-		int num[] = new int [150];
+		int num[] = new int [10];
+		int par = 0;
+		
+		int limite= (100-(-100));
+		
+		for (int i=0; i<num.length; i++) {
+			num[i]=(int)Math.round(Math.random()*limite+-(-100));
+			System.out.print(num[i]+" ");
+			
+			
+			
+			//EN EL CASO DE QUE SEA PAR Y POSITIVO
+			if (num[i]%2==0 && num[i]>0) {
+				
+				//MULTIPLICAMOS POR -1 PARA QUE SEAN NEGATIVOS
+				
+				par=num[i]*(-1);
+				
+				System.out.print(par+" ");
+														
+			}
+			
+			else {
+				par=num[i]*(-1);
+				
+				System.out.print(par+" ");
+							
+			}
+			System.out.println("\n");
+		}
+		
+
+	
 
 	}
 
---
+++ EjerciciosJava/src/capitulo05/bloque02/Ejercicio02_3_Arrays.java
@@ -0,0 +1,39 @@
+package capitulo05.bloque02;
+
+public class Ejercicio02_3_Arrays {
+
+	public static void main(String[] args) {
+		
+		int array1[]= new int [5];
+		int array2[]= new int [5];
+		int array3[]= new int [5];
+		int impar=0;
+		
+		for (int i=0; i<array1.length; i++) {
+			array1[i]=numAleatorio();
+			System.out.print(array1+" ");
+			
+			if (i%2!=0) {
+				impar=array1[i];
+			}
+		}	
+			
+		for (int j=0; j<array3.length; j++) {
+			array3[j]=numAleatorio();
+			
+			System.out.print(array3+" ");
+			
+				if (j%2!=0) {
+					array3[j]=impar;
+				}
+			
+		}
+		System.out.println(array3);
+	}
+	
+	
+	private static int numAleatorio() {
+		return (int) Math.round(Math.random()*100);
+	}
+
+}
