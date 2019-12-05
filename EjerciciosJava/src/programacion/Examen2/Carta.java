544ff28dd3d7eb199e01b6f5e904e8af93e0df2e
diurno <diurno@pc-aula>
2019-12-05 15:01:53 +0100

    jj

---
+++ EjerciciosJava/src/programacion/Examen2/Carta.java
@@ -0,0 +1,86 @@
+package programacion.Examen2;
+
+public class Carta {
+	private int valor;
+	private String palo;
+	private String descripcion;
+	private int id;
+	
+	public Carta () {
+		
+	}
+	
+	/**
+	 * @param valor
+	 * @param palo
+	 * @param descripcion
+	 * @param id
+	 */
+	public Carta(int valor, String palo, String descripcion, int id) {
+		super();
+		this.valor = valor;
+		this.palo = palo;
+		this.descripcion = descripcion;
+		this.id = id;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public int getValor() {
+		return valor;
+	}
+	/**
+	 * @param valor the valor to set
+	 */
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	/**
+	 * @return the palo
+	 */
+	public String getPalo() {
+		return palo;
+	}
+	/**
+	 * @param palo the palo to set
+	 */
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+	/**
+	 * @return the descripcion
+	 */
+	public String getDescripcion() {
+		return descripcion;
+	}
+	/**
+	 * @param descripcion the descripcion to set
+	 */
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Carta [valor=" + valor + ", palo=" + palo + ", descripcion=" + descripcion + ", id=" + id + "]\n";
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ EjerciciosJava/src/programacion/Examen2/Principal.java
@@ -0,0 +1,13 @@
+package programacion.Examen2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Baraja baraja = new Baraja();
+		
+		System.out.println(baraja.toString());
+		
+	}
+
+}
