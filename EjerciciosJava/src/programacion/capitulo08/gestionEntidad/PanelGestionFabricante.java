5f203f18365bd71df55c6dba9ccb9d23d561bef6
nuriaRZ <nuriesma99@gmail.com>
2020-03-12 14:43:05 +0100

    a

--- EjerciciosJava/src/programacion/capitulo08/gestionEntidad/PanelGestionFabricante.java
+++ EjerciciosJava/src/programacion/capitulo08/gestionEntidad/PanelGestionFabricante.java
@@ -1,7 +1,13 @@
 package programacion.capitulo08.gestionEntidad;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import modelo.ventaDeCoches.Fabricante;
 
 public class PanelGestionFabricante extends JPanel {
 	public static int LOAD_FIRST = 0;
@@ -13,4 +19,27 @@ public class PanelGestionFabricante extends JPanel {
 	public static int REMOVE = 6;
 	
 	JTextField jtfId = new JTextField(5);
+	JTextField jtfCif = new JTextField(15);
+	JTextField jtfNombre = new JTextField(40);
+	
+	Fabricante actual = null;
+	
+	public PanelGestionFabricante() {
+		
+	}
+	private void construir() {
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 2;
+		c.insets = new Insets(0, 0, 25, 0);
+		
+	}
+	
+	private Jpanel getPanelNavegacion() {
+		JPanel pnl = new JPanel();
+	}
 }
